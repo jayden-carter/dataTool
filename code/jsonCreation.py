8903b5a6cd30cf51314266d69bb40db98045887a
jayden-carter <jaydencarter@ucsb.edu>
2025-04-08 07:34:57 -0700

    Update jsonCreation.py

--- code/jsonCreation.py
+++ code/jsonCreation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("../data/shopping.csv")  # Read CSV
-df.to_json("../data/shopping.json", orient="records", indent=4)  # Convert to JSON
+df = pd.read_csv("/Users/jaydencarter/Desktop/dataTool/data/shopping.csv")  # Read CSV
+df.to_json("/Users/jaydencarter/Desktop/dataTool/data/shopping.json", orient="records", indent=4)  # Convert to JSON
 
 print("CSV successfully converted to JSON!")
+
+

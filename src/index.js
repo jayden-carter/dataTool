00776440862f17e089124870167dd1cffb9cfe0a
jayden-carter <jaydencarter@ucsb.edu>
2025-04-18 11:15:36 -0700

    install node.js and firebase

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyDcacpYTrShmiUcGm8NgXucH6IRNlr3L5E",
+    authDomain: "synq-data.firebaseapp.com",
+    projectId: "synq-data",
+    storageBucket: "synq-data.firebasestorage.app",
+    messagingSenderId: "666582373055",
+    appId: "1:666582373055:web:48a69368feb8c16ab52c0a",
+    measurementId: "G-2E8MH39H3E"
+  });
